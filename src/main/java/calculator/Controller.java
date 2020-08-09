36fe565c383bdc7470d1c6cff5d0e642e9be364b
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-08-09 19:46:51 +0200

    Assign on click action to all buttons
    Action iterate over buttons list and assign action
    depending on kind of button. There are free options.
    Cancel button clear output text field and string builder.
    Equal sign button set up provided numbers to output text
    field. Numeric and math sign buttons set up its value
    to input field.

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -1,4 +1,52 @@
 package calculator;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    @FXML private GridPane mainGridPane;
+    @FXML private TextField inputField;
+    @FXML private TextField outputField;
+    private StringBuilder builder = new StringBuilder();
+
+    public void initialize(){
+        assignOnClickEventToAllButtons();
+    }
+
+    public void assignOnClickEventToAllButtons(){
+
+        ObservableList<Node> buttons = mainGridPane.getChildren();
+        for (Node button : buttons) {
+
+            Button newButton = (Button) button;
+            newButton.setOnMouseClicked(e -> {
+                if (newButton.isCancelButton()) {
+                    clearTextField();
+                } else if (newButton.getText().equals("=")){
+                    assignTextToOutputField();
+                } else {
+                    assignTextToInputField(newButton);
+                }
+            });
+        }
+    }
+
+    private void assignTextToOutputField(){
+        outputField.setText(builder.toString());
+    }
+
+    private void assignTextToInputField(Button button) {
+        builder.append(button.getText());
+        inputField.setText(builder.toString());
+    }
+
+    private void clearTextField(){
+        builder.delete(0, builder.length());
+        inputField.setText(null);
+    }
 }
